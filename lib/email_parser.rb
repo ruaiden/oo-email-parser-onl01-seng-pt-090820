52f1e50918147709295c604a34cfd561c2e3a32b
Aiden Marques <aiden.marques94@gmail.com>
2020-09-30 14:59:05 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
+class EmailAddressParser
+  
+  attr_accessor :email
+
+  def initialize(email)
+     @email=email
+  end 
+  
+  def parse
+      email.split.collect do |emails| 
+        emails.split(",")
+    end
+    ss.flatten.uniq
+  end 
+  end 
+  
+ 
+
+
+
+
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
